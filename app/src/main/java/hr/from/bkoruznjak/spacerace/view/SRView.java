f345ca8756f08e3e6caf613e6b95d763f088b030
Borna Koruznjak <bkoruznjak@realnetworks.com>
2017-01-26 23:05:51 +0100

    - implemented FPS

--- app/src/main/java/hr/from/bkoruznjak/spacerace/view/SRView.java
+++ app/src/main/java/hr/from/bkoruznjak/spacerace/view/SRView.java
@@ -19,12 +19,18 @@ import hr.from.bkoruznjak.spacerace.model.SpaceShip;
 
 public class SRView extends SurfaceView implements Runnable, SRControl, GameControl {
 
+    private static final int TARGET_FPS = 60;
     private Thread gameThread = null;
     private SurfaceHolder mSurfaceHolder;
     private Canvas mScreenCanvas;
     private Paint mBackgroundColor;
 
     private SpaceShip mPlayerShip;
+    private float mTargetFrameDrawTime;
+    private long mStartTimeCurrentFrame;
+    private long mDelta;
+    private long mEndTimeCurrentFrame;
+    private long mSleepTimeInMillis;
     private volatile boolean playing;
 
     public SRView(Context context) {
@@ -49,6 +55,7 @@ public class SRView extends SurfaceView implements Runnable, SRControl, GameCont
     }
 
     private void init(Context context) {
+        this.mTargetFrameDrawTime = 1000f / TARGET_FPS;
         this.mSurfaceHolder = getHolder();
         this.mBackgroundColor = new Paint();
         this.mPlayerShip = new SpaceShip
@@ -79,7 +86,8 @@ public class SRView extends SurfaceView implements Runnable, SRControl, GameCont
     @Override
     public void draw() {
         if (mSurfaceHolder.getSurface().isValid()) {
-
+            //Get start time for FPS calcualtion
+            mStartTimeCurrentFrame = System.nanoTime() / 1000000;
             //First we lock the area of memory we will be drawing to
             mScreenCanvas = mSurfaceHolder.lockCanvas();
 
@@ -95,14 +103,20 @@ public class SRView extends SurfaceView implements Runnable, SRControl, GameCont
 
             // Unlock and draw the scene
             mSurfaceHolder.unlockCanvasAndPost(mScreenCanvas);
+            //Get end time for FPS calcualtion
+            mEndTimeCurrentFrame = System.nanoTime() / 1000000;
         }
-
     }
 
     @Override
     public void control() {
         try {
-            gameThread.sleep(17);
+            //calculate FPS
+            mDelta = mEndTimeCurrentFrame - mStartTimeCurrentFrame;
+            mSleepTimeInMillis = (long) (mTargetFrameDrawTime - mDelta);
+            if (mSleepTimeInMillis > 0) {
+                gameThread.sleep(mSleepTimeInMillis);
+            }
         } catch (InterruptedException e) {
             Log.e("bbb", "InterruptedException:" + e);
         }
