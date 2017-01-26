d02f9da45664b78de8601e90b46b0659b484d4a1
Borna Koruznjak <bkoruznjak@realnetworks.com>
2017-01-26 20:25:15 +0100

    - added game activity

--- app/src/main/java/hr/from/bkoruznjak/spacerace/MainActivity.java
+++ app/src/main/java/hr/from/bkoruznjak/spacerace/MainActivity.java
@@ -1,7 +1,11 @@
 package hr.from.bkoruznjak.spacerace;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button newGameButton = (Button) findViewById(R.id.button_start_game);
+
+        TextView textHighScore = (TextView) findViewById(R.id.text_high_score);
+        textHighScore.setText(textHighScore.getText().toString().concat("00000000"));
+
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startGameIntent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(startGameIntent);
+                finish();
+            }
+        });
     }
 }
