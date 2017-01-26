c1d7a81c5cc67a7fec04e0406aebb932da143f0f
Borna Koruznjak <bkoruznjak@realnetworks.com>
2017-01-26 21:33:56 +0100

    - added drawing  - were flying

--- app/src/main/java/hr/from/bkoruznjak/spacerace/view/SRView.java
+++ app/src/main/java/hr/from/bkoruznjak/spacerace/view/SRView.java
@@ -2,9 +2,16 @@ package hr.from.bkoruznjak.spacerace.view;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+
+import hr.from.bkoruznjak.spacerace.R;
+import hr.from.bkoruznjak.spacerace.model.SpaceShip;
 
 /**
  * Created by bkoruznjak on 26/01/2017.
@@ -12,24 +19,45 @@ import android.view.SurfaceView;
 
 public class SRView extends SurfaceView implements Runnable, SRControl, GameControl {
 
-    Thread gameThread = null;
+    private Thread gameThread = null;
+    private SurfaceHolder mSurfaceHolder;
+    private Canvas mScreenCanvas;
+    private Paint mBackgroundColor;
+
+    private SpaceShip mPlayerShip;
     private volatile boolean playing;
 
     public SRView(Context context) {
         super(context);
+        init(context);
     }
 
     public SRView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init(context);
     }
 
     public SRView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init(context);
     }
 
     @TargetApi(21)
     public SRView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init(context);
+    }
+
+    private void init(Context context) {
+        this.mSurfaceHolder = getHolder();
+        this.mBackgroundColor = new Paint();
+        this.mPlayerShip = new SpaceShip
+                .Builder(context)
+                .bitmap(R.drawable.ship)
+                .speed(50)
+                .x(50)
+                .y(50)
+                .build();
     }
 
     @Override
@@ -45,16 +73,39 @@ public class SRView extends SurfaceView implements Runnable, SRControl, GameCont
 
     @Override
     public void update() {
-
+        mPlayerShip.update();
     }
 
     @Override
     public void draw() {
+        if (mSurfaceHolder.getSurface().isValid()) {
+
+            //First we lock the area of memory we will be drawing to
+            mScreenCanvas = mSurfaceHolder.lockCanvas();
+
+            // Rub out the last frame
+            mScreenCanvas.drawColor(Color.argb(255, 0, 0, 0));
+
+            // Draw the player
+            mScreenCanvas.drawBitmap(
+                    mPlayerShip.getBitmap(),
+                    mPlayerShip.getX(),
+                    mPlayerShip.getY(),
+                    mBackgroundColor);
+
+            // Unlock and draw the scene
+            mSurfaceHolder.unlockCanvasAndPost(mScreenCanvas);
+        }
 
     }
 
     @Override
     public void control() {
+        try {
+            gameThread.sleep(17);
+        } catch (InterruptedException e) {
+            Log.e("bbb", "InterruptedException:" + e);
+        }
 
     }
 
