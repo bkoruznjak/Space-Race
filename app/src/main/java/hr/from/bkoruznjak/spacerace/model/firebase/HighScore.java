da899f7cd70848059295bf3cdaceeaec71489c84
Borna Koruznjak <bkoruznjak@realnetworks.com>
2017-01-29 02:50:40 +0100

    - removed ID from highscore as we set it on the scores child

--- app/src/main/java/hr/from/bkoruznjak/spacerace/model/firebase/HighScore.java
+++ app/src/main/java/hr/from/bkoruznjak/spacerace/model/firebase/HighScore.java
@@ -1,14 +1,11 @@
 package hr.from.bkoruznjak.spacerace.model.firebase;
 
-import java.util.UUID;
-
 /**
  * Created by bkoruznjak on 29/01/2017.
  */
 
 public class HighScore {
 
-    private String id;
     private String alias;
     private long score;
 
@@ -16,17 +13,8 @@ public class HighScore {
     }
 
     public HighScore(String alias, long score) {
-        this.id = UUID.randomUUID().toString();
         this.alias = alias;
         this.score = score;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
     }
 
     public String getAlias() {
