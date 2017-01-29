d3d6bcefe9c2cd9830a1286534a1b8fbba586165
Borna Koruznjak <bkoruznjak@realnetworks.com>
2017-01-29 11:58:15 +0100

    - added alias alert dialog

--- app/src/main/java/hr/from/bkoruznjak/spacerace/MainActivity.java
+++ app/src/main/java/hr/from/bkoruznjak/spacerace/MainActivity.java
@@ -1,12 +1,16 @@
 package hr.from.bkoruznjak.spacerace;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -64,5 +68,31 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(startGameIntent);
             }
         });
+
+        AlertDialog.Builder aliasDialogBuilder = new AlertDialog.Builder(this);
+        aliasDialogBuilder.setView(R.layout.dialog_alias);
+
+        aliasDialogBuilder.setPositiveButton("Commit", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                EditText userInput = (EditText) ((AlertDialog) dialog).findViewById(R.id.alias);
+                Log.d("bbb", "confirmed alias:" + userInput.getText().toString());
+            }
+        });
+        final AlertDialog aliasDialog = aliasDialogBuilder.create();
+        aliasDialog.getWindow().setBackgroundDrawable(getResources().getDrawable(R.drawable.shape_alias));
+        aliasDialog.setOnShowListener(new DialogInterface.OnShowListener() {
+            @Override
+            public void onShow(DialogInterface arg0) {
+                aliasDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(getResources().getColor(R.color.aliasColor));
+                EditText userInput = (EditText) aliasDialog.findViewById(R.id.alias);
+                userInput.getBackground().mutate().setColorFilter(getResources().getColor(R.color.aliasColor), PorterDuff.Mode.SRC_ATOP);
+
+            }
+        });
+        aliasDialog.setCanceledOnTouchOutside(false);
+        aliasDialog.show();
+
+
     }
 }
